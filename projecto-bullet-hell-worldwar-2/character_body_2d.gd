6a58db6a984c2f00d037cc9d22801b1a3a3c6d09
santiagoritter <santiagoritter26@gmail.com>
2026-03-30 17:48:59 -0300

    hacer personaje

--- projecto-bullet-hell-worldwar-2/character_body_2d.gd
+++ projecto-bullet-hell-worldwar-2/character_body_2d.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 var velocidad = 400.0
 
+func _Atacar():
+	if Input.is_physical_key_pressed(KEY_SPACE):
+		$AnimatedSprite2D.play("Espada")
+
+
 func _physics_process(delta):
 	var direccion_Y = 0
 	var direccion_X = 0
@@ -14,8 +19,11 @@ func _physics_process(delta):
 		direccion_X -= 1
 	if Input.is_physical_key_pressed(KEY_D):
 		direccion_X += 1
+	if Input.is_physical_key_pressed(KEY_SPACE):
+		$AnimatedSprite2D.play("Espada")
 	
-	
+	if direccion_X == 0:
+		$AnimatedSprite2D.play("Quieto")
 		
 	velocity.y = direccion_Y * velocidad
 	velocity.x = direccion_X * velocidad
