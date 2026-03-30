fd9f4cac83b8aea6e1c2b11a585a63f418553a4e
santiagoritter <santiagoritter26@gmail.com>
2026-03-30 17:28:16 -0300

    first commt

---
+++ projecto-bullet-hell-worldwar-2/character_body_2d.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var velocidad = 400.0
+
+func _physics_process(delta):
+	var direccion_Y = 0
+	var direccion_X = 0
+	
+	if Input.is_physical_key_pressed(KEY_W):
+		direccion_Y -= 1
+	if Input.is_physical_key_pressed(KEY_S):
+		direccion_Y += 1
+	if Input.is_physical_key_pressed(KEY_A):
+		direccion_X -= 1
+	if Input.is_physical_key_pressed(KEY_D):
+		direccion_X += 1
+	
+	
+		
+	velocity.y = direccion_Y * velocidad
+	velocity.x = direccion_X * velocidad
+	move_and_slide()
